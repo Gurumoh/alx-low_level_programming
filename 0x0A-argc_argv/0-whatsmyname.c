3594f910c434ab6ba33103c7257a27f88fde2823
gurumoh <ayinlaibrahim133@gmail.com>
2022-07-23 12:56:45 -0700

    m

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is a function that prints the name.
+ *@argc: is an argument counter for char.
+ *@argv: is an agument value for char.
+ *Return: Returns always success.
+ **/
+
+int main(int argc, char *argv[])
+{
+if (argc == 0)
+{
+printf("%s\n", argv[argc]);
+}
+return (0);
+}
