cd4a9b3f12ddf134f505a5adbc98c6d2c1c5dd00
Gurumoh <83771348+Gurumoh@users.noreply.github.com>
2022-07-24 01:43:07 +0100

    Commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,13 @@
  * @argc: is a counter of argument for int.
  * @argv: is a counter for value of the argument to char
  * Return: Returns always success.
- **/
+**/
 
 int main(int argc, char *argv[])
 {
-int i = argc;
+(void)argv;
 
-printf("%d\n", argc);
+printf("%d\n", (argc - 1));
 
 return (0);
 }
