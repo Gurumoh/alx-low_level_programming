9dbb3580293b3830478733cfcccd0160f37bf5cc
gurumoh <ayinlaibrahim133@gmail.com>
2022-07-28 14:29:43 -0700

    m

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int* i;
+
+ i = malloc(sizeof(int) * 2);
+
+malloc_checked(5)
+
+free(i)
+}
